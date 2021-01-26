9eb53e1cb58bb928d07f5c43369c240e78a9ace1
Mike Didomizio <mikedidomizio@gmail.com>
2021-01-26 11:38:20 -0500

    Add unit test integration

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
+  // commented out because Jest was complaining about it, not entirely sure if still required
+  // "presets": ['@vue/cli-plugin-babel/preset'],
+
+  // required for Jest
+  "env": {
+    "test": {
+      "presets": [["env", { "targets": { "node": "current" } }]]
+    }
+  }
 }
---
+++ src/components/__tests__/CVItem.spec.js
@@ -0,0 +1,34 @@
+import {shallowMount} from '@vue/test-utils'
+
+import Component from '../CVItem.vue';
+
+describe('CVItem', () => {
+
+    let wrapper;
+
+    beforeAll(() => {
+        wrapper = shallowMount(Component, {
+            propsData: {
+                education: false,
+                website: 'http://google.ca',
+                title: 'Google',
+            }
+        })
+    });
+
+    describe('title', () => {
+
+        it('should not have a website link if education is true', async() => {
+            await wrapper.setProps({'education': true});
+            const element = wrapper.find('h6');
+            expect(element.html()).toBe('<h6>Google</h6>')
+        });
+
+        it('should have a company website link if education is false', async() => {
+            await wrapper.setProps({'education': false});
+            const element = wrapper.find('h6');
+            expect(element.html()).toBe('<h6><a href="http://google.ca">Google</a></h6>');
+        });
+
+    });
+});
