8803aee5aeef706f45c86c236c4214267d7442fb
Mike DiDomizio <mikedidomizio@gmail.com>
2021-05-05 18:45:16 -0400

    Simplify the Docker build process for
    Easily get the CI build images in Windows through the ci-test bash
    script which means GitHub actions should no longer require building the
    Docker image

---
+++ scripts/ci-test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# this file was designed so someone could quickly get the CI images and ensure that the build will pass in CI
+
+# runs tests against the pre-built docker image
+winpty docker run -it --rm -v /"$PWD"/src/__tests__:/usr/src/app/src/__tests__ -it mikedidomizio.com:latest //bin//bash -c "pm2 start yarn --wait-ready --interpreter bash --name app -- serve; yarn ci:test"
