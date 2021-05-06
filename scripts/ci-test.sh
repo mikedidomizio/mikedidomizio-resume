d53d4ffd9bfbdabdb62e4576e6deb9ab44c75e79
Mike DiDomizio <mikedidomizio@gmail.com>
2021-05-06 09:25:55 -0400

    Use Docker image in GH actions

--- scripts/ci-test.sh
+++ scripts/ci-test.sh
@@ -3,4 +3,4 @@
 # this file was designed so someone could quickly get the CI images and ensure that the build will pass in CI
 
 # runs tests against the pre-built docker image
-winpty docker run -it --rm -v /"$PWD"/src/__tests__:/usr/src/app/src/__tests__ -it mikedidomizio.com:latest //bin//bash -c "pm2 start yarn --wait-ready --interpreter bash --name app -- serve; yarn ci:test"
+winpty docker run -it --rm -v /"$PWD"/src/__tests__:/usr/src/app/src/__tests__ mikedidomizio.com:latest //bin//bash -c "pm2 start yarn --wait-ready --interpreter bash --name app -- serve; yarn ci:test"
