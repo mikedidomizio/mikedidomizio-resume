088265a26a2942b6b7fd12182567527109dea9e5
Mike DiDomizio <mikedidomizio@gmail.com>
2021-05-29 20:09:04 -0400

    Add script files

---
+++ scripts/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# ensure a clean build in the volume
+rm -rf ./dist
+# we build into a different directory and then move the output to the ./dist directory
+# if you just do to dist there is a locked file issue
+docker run -i --rm -v "${PWD}/src":/usr/src/app/src -v "${PWD}/dist":/usr/src/app/dist "$1" /bin/bash -c "yarn build --dest ./tmp/dist; cp -r ./tmp/dist ./"
+
