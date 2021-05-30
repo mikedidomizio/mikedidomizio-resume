02f0714d6747f1ff4d8a33fd2ef29596c19c95c9
Mike DiDomizio <mikedidomizio@gmail.com>
2021-05-30 16:35:09 -0400

    Do PDF testing to check number of pages and page content

---
+++ src/__tests__/App.pdf.spec.js
@@ -0,0 +1,64 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const PdfReader = require('pdfreader').PdfReader;
+
+xdescribe('PDF generating test', () => {
+    let browser;
+    const pdfFile = 'tmp/generated.pdf';
+    const maxNumberOfPages = 2;
+
+    it(`PDF should be less than ${maxNumberOfPages} pages long`, async () => {
+        browser = await puppeteer.launch();
+        const page = await browser.newPage();
+
+        // create the directory if it doesn't exist
+        const dir = './tmp';
+        if (!fs.existsSync(dir)){
+            fs.mkdirSync(dir);
+        }
+
+        await page.goto('http://localhost:8080');
+        await page.pdf({path: pdfFile, format: 'letter'});
+
+        const pdfBuffer = fs.readFileSync(pdfFile);
+        const pdfData = await new Promise((resolve, /*reject*/) => {
+            let numberOfPages = 0;
+            // page height is 49.5 for letter format
+            let pageHeight = 49.5;
+            let lastPageItem = 0;
+            new PdfReader().parseBuffer(pdfBuffer, function (err, item) {
+                if (item) {
+                    // is (new) page metadata
+                    if (item.page) {
+                        numberOfPages = item.page;
+                        pageHeight = item.height;
+                    }
+                    // is something on the page
+                    if (item.text) {
+                        lastPageItem = item.y;
+                    }
+                }
+
+                // is done
+                if (!item) {
+                    resolve({
+                        lastPageItem,
+                        pageHeight,
+                        numberOfPages
+                    });
+                }
+            });
+        });
+
+        // in case there is something wrong we want to ensure the PDF has rendered at least something
+        expect(pdfData.numberOfPages).toBeGreaterThan(0);
+
+        // the CV should be either less than the max number of pages
+        // or if it is one more than the max, it should at least fill a good chunk of the page
+        // so it's not mostly an empty page
+        expect(pdfData.numberOfPages <= maxNumberOfPages ||
+            (pdfData.lastPageItem >= pdfData.pageHeight / 8 && pdfData.numberOfPages <= maxNumberOfPages + 1)).toBeTruthy();
+    });
+
+    afterAll(async () => await browser.close());
+});
