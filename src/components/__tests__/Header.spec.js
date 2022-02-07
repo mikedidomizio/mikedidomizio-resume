a7c9fcac147a059523caca6247c4299617f88d95
Mike DiDomizio <mikedidomizio@gmail.com>
2022-02-06 22:50:22 -0500

    Add header test, website link now opens in new tab

--- src/components/__tests__/Header.spec.js
+++ src/components/__tests__/Header.spec.js
@@ -9,14 +9,24 @@ describe('Header', () => {
   beforeAll(() => {
     wrapper = shallowMount(Component, {
       propsData: {
-        email: 'test@test.com'
+        email: 'test@test.com',
+        website: 'google.ca',
       }
     })
   });
 
   it('should return an email link', async() => {
-    const element = wrapper.find('a');
+    const element = wrapper.find('a.email');
     expect(element.attributes().href).toBe('mailto:test@test.com')
+  });
+
+  describe('website', () => {
+
+    it('should be displayed', () => {
+      const element = wrapper.find('a.website');
+      expect(element.attributes().href).toBe('http://google.ca')
+    });
+
   });
 
 });
