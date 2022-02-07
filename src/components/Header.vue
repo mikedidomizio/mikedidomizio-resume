9ba6a293d251339dabe8bd97bfabf7a6ee7a130f
Mike DiDomizio <mikedidomizio@gmail.com>
2022-02-06 22:09:30 -0500

    Add GitHub link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -21,8 +21,10 @@
         <div class="span3">
 
             <ul class="contact-info hide-on-phone fixed">
-                <li>E: <a :href="formattedEmail()">{{email}}</a></li>
-                <li>T: <strong>{{phoneNumber}}</strong></li>
+                <li class="text"><span>E:</span> <a :href="formattedEmail()">{{email}}</a></li>
+                <li class="text"><span>T:</span> <strong>{{phoneNumber}}</strong></li>
+                <li class="text"><span>W:</span> <a :href="formattedWebsite()">{{website}}</a></li>
+                <li class="img"><span><img src="@/assets/GitHub-Mark-32px.png" /></span> <a :href="'https://www.github.com/' + github" target="_blank">{{github}}</a></li>
             </ul>
 
         </div><!-- end .span3 -->
@@ -35,12 +37,32 @@
     methods: {
       formattedEmail() {
         return `mailto:${this.email}`;
+      },
+      formattedWebsite() {
+        return `http://${this.website}`;
       }
     },
-    props: ['city', 'email', 'name', 'phoneNumber', 'position', 'postalCode'],
+    props: ['city', 'email', 'github', 'name', 'phoneNumber', 'position', 'postalCode', 'website'],
     data() {
       // This solely exists to appear in the coverage report
       return {}
     }
   }
 </script>
+
+<style scoped>
+  img  {
+    height: 16px;
+    width: 16px;
+    margin-left: -2px;
+  }
+
+  .contact-info .img a {
+    vertical-align: text-bottom;
+  }
+
+  .contact-info span {
+    text-align: center;
+    width: 16px;
+  }
+</style>
